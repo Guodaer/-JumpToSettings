dd9538b850d346e08cabbe4b5997aa125e203027
GD1207 <1023251767@qq.com>
2016-01-27 18:00:13 +0800

    Initial Commit

---
+++ 跳转到设置/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  跳转到设置
+//
+//  Created by xiaoyu on 16/1/27.
+//  Copyright © 2016年 Guoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 跳转到设置/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  跳转到设置
+//
+//  Created by xiaoyu on 16/1/27.
+//  Copyright © 2016年 Guoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
