566b463ad4cc7135677c429af717f15ab1132b91
GD1207 <1023251767@qq.com>
2016-01-27 18:47:16 +0800

    从应用内部跳转到系统设置中的位置
    跳转到系统设置中的任意位置，里面有路径自己可以替换

--- 跳转到设置/ViewController.m
+++ 跳转到设置/ViewController.m
@@ -17,8 +17,57 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeSystem];
+    button.frame = CGRectMake(100, 100, 100, 30);
+    [button setTitle:@"按钮" forState:UIControlStateNormal];
+    [button addTarget:self action:@selector(btn) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:button];
 }
+- (void)btn {
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"prefs:root=General"]];
 
+}
+#if 0
+About — prefs:root=General&path=About
+Accessibility — prefs:root=General&path=ACCESSIBILITY
+Airplane Mode On — prefs:root=AIRPLANE_MODE
+Auto-Lock — prefs:root=General&path=AUTOLOCK
+Brightness — prefs:root=Brightness
+Bluetooth — prefs:root=General&path=Bluetooth
+Date & Time — prefs:root=General&path=DATE_AND_TIME
+FaceTime — prefs:root=FACETIME
+General — prefs:root=General
+Keyboard — prefs:root=General&path=Keyboard
+iCloud — prefs:root=CASTLE
+iCloud Storage & Backup — prefs:root=CASTLE&path=STORAGE_AND_BACKUP
+International — prefs:root=General&path=INTERNATIONAL
+Location Services — prefs:root=LOCATION_SERVICES
+Music — prefs:root=MUSIC
+Music Equalizer — prefs:root=MUSIC&path=EQ
+Music Volume Limit — prefs:root=MUSIC&path=VolumeLimit
+Network — prefs:root=General&path=Network
+Nike + iPod — prefs:root=NIKE_PLUS_IPOD
+Notes — prefs:root=NOTES
+Notification — prefs:root=NOTIFICATIONS_ID
+Phone — prefs:root=Phone
+Photos — prefs:root=Photos
+Profile — prefs:root=General&path=ManagedConfigurationList
+Reset — prefs:root=General&path=Reset
+Safari — prefs:root=Safari
+Siri — prefs:root=General&path=Assistant
+Sounds — prefs:root=Sounds
+Software Update — prefs:root=General&path=SOFTWARE_UPDATE_LINK
+Store — prefs:root=STORE
+Twitter — prefs:root=TWITTER
+Usage — prefs:root=General&path=USAGE
+VPN — prefs:root=General&path=Network/VPN
+Wallpaper — prefs:root=Wallpaper
+Wi-Fi — prefs:root=WIFI
+
+
+#endif
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
